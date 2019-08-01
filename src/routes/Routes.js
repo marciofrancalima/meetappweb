6dc0ab2e28a84fed22df41f283a01e8e55a2fe05
Márcio França Lima <marciofrancalima@hotmail.com>
2019-08-01 10:26:55 -0300

    Created user module to get logged profile data and configured redux-persist to store user token in localStorage

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom';
 import authLayout from '~/pages/_layouts/auth';
 import defaultLayout from '~/pages/_layouts/default';
 
-import store from '~/store';
+import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
