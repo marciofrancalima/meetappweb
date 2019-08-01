3a05990500668b3f47d31646a02bc8fcda6b9025
Márcio França Lima <marciofrancalima@hotmail.com>
2019-08-01 09:54:58 -0300

    Configured Redux/Saga and user authentication via api

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,28 @@
+import { takeLatest, all, call, put } from 'redux-saga/effects';
+
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess, signInFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    yield put(signInSuccess(token, user));
+    history.push('/dashboard');
+  } catch (error) {
+    console.tron.lon('Usuário não conseguiu logar');
+  } finally {
+    yield put(signInFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
