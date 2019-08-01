40fa961713f956ce4b9c7aaae07a231f96cfcd1e
Márcio França Lima <marciofrancalima@hotmail.com>
2019-07-31 21:18:52 -0300

    Stylized default layout for SignIn and SignUp components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="Meetapp" />
+
+      <form>
+        <input type="email" placeholder="Digite seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta grátis</Link>
+      </form>
+    </>
+  );
 }
