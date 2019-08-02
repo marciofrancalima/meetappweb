ca9db00884edf26c237924ea886f769f128332a1
Márcio França Lima <marciofrancalima@hotmail.com>
2019-08-02 19:50:05 -0300

    Added user token in localStorage

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 // import { Container } from './styles';
+import api from '~/services/api';
 
 export default function Dashboard() {
+  api.get('meetups');
   return <h1>Dashboard</h1>;
 }
